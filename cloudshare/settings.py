3a0759a9a8dc9580a37c12c75047fc7c8d4ce0ec
David Barrow <dave@fusion-arc.uk>
2020-03-29 15:50:54 +0100

    update

--- cloudshare/settings.py
+++ cloudshare/settings.py
@@ -92,6 +92,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
