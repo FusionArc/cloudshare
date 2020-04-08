47116c0cbf516fc170d7f54fe33a646a944da958
David Barrow <dave@fusion-arc.uk>
2020-04-08 04:45:00 +0100

    Add Docker builds

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Award(models.Model):
+    title - models.CharField(max_length=60)
+    img = models.ImageField(upload_to='images')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.title = None
+
+        def __str__(self):
+            return self.title
+
